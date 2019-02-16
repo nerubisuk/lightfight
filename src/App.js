9eb622f2f08fd3b5359714081b9737c9e24b8c0f
Roman K <roman@obraz.es>
2019-02-16 21:32:09 +0200

    Injects  <VoteButtons>

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './styles/base.scss';
 import NavBar from './components/NavBar';
 import Header from './components/Header';
+import VoteButtons from './components/VoteButtons';
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Header />
+        <main>
+          <VoteButtons />
+        </main>
         <NavBar />
       </>
     );
