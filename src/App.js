a07b5b3528f6e0db553385e4bf68c3ee648ef180
Roman K <roman@obraz.es>
2019-02-16 20:46:39 +0200

    Injects header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './styles/base.scss';
 import NavBar from './components/NavBar';
+import Header from './components/Header';
 
 class App extends React.Component {
   render() {
     return (
       <>
+        <Header />
         <NavBar />
       </>
     );
