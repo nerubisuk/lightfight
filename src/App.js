66b3357e44d9f95cf1358cd923954273630925c0
Roman K <roman@obraz.es>
2019-02-16 18:55:18 +0200

    Refactors for better structure

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import NavBar from './components/NavBar';
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
+      <>
         <NavBar />
-      </div>
+      </>
     );
   }
 }
