6097f6d2ea53b6fe9151dad4c0caea771886a713
Roman K <roman@obraz.es>
2019-02-16 18:51:11 +0200

    Injects <NavBar> component to layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './styles/base.scss';
+import NavBar from './components/NavBar';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">App</div>
+      <div className="App">
+        <NavBar />
+      </div>
     );
   }
 }
