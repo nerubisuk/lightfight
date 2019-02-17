dd7e4226467cd5144e5aa9c23ca31115a033bcaa
Roman K <roman@obraz.es>
2019-02-17 13:59:17 +0200

    Pass props to <Versus>. Adds props checking

--- src/components/Versus.js
+++ src/components/Versus.js
@@ -4,17 +4,23 @@
  * @author Anton Komarenko <mi3ta@sent.as>
  */
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from '../styles/components/Versus.module.scss';
 
 /* Component definition */
-const Versus = () => 
+const Versus = ({ rivals }) => 
   <div className={styles.wrapper}>
     <div>
-      <img src="/images/dota.jpg" alt="first" />
+      <img src={rivals[0].image} alt="first" />
     </div>
     <div>
-      <img src="/images/fortnite.jpg" alt="second" />
+      <img src={rivals[1].image} alt="second" />
     </div>
   </div>;
+
+/* Prop types definition */
+Versus.propTypes = {
+  rivals: PropTypes.array.isRequired
+}
 
 export default Versus;
