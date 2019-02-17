5f3efa2877842cffedcd5797a4fee2789ec46e1d
Roman K <roman@obraz.es>
2019-02-17 18:05:03 +0200

    Adds click buttons sound

--- src/components/VoteButtons.js
+++ src/components/VoteButtons.js
@@ -5,14 +5,29 @@
  */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Howl } from 'howler';
 import styles from '../styles/components/VoteButtons.module.scss';
 
 /* Component definition */
-const VoteButtons = ({ rivals }) => 
-  <div className={styles.wrapper}>
-    <button>{rivals[0].name}</button>
-    <button>{rivals[1].name}</button>
-  </div>;
+const VoteButtons = ({ rivals }) => {
+  const handleClick = () => {
+    const click = new Howl({
+      src: ['/sounds/click.mp3', '/sounds/click.ogg']
+    });
+
+    const hover = new Howl({
+      src: ['/sounds/hover.mp3', '/sounds/hover.ogg']
+    })
+
+    click.play();
+    hover.play();
+  }
+
+  return <div className={styles.wrapper}>
+    <button onClick={handleClick}>{rivals[0].name}</button>
+    <button onClick={handleClick}>{rivals[1].name}</button>
+  </div>
+}
 
 /* Prop types definition */
 VoteButtons.propTypes = {
